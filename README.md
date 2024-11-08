46c8957749862d5f55f48a45f2f5149081148ad6
Shyam Singh <shyamsingh841442@gmail.com>
2024-11-08 14:40:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,122 @@
+# MovieMania
+  MovieMania is a React-based web application that provides information on popular, top-rated, and upcoming movies. Users can view movie details, including ratings, synopsis, and release dates, and explore different movie categories. This project uses **The Movie Database (TMDb) API** to fetch and display data dynamically.
+
+
+
+ ## Table of Contents
+- [Live Demo](#live-demo)
+- [Features](#features)
+- [Installation](#installation)
+- [Technologies Used](#technologies-used)
+- [Project Structure](#project-structure)
+- [Usage](#usage)
+- [Project Repository](#project-repository)
+- [Credits](#credits)
+- [License](#license)
+- [Contact Information](#contact-information)
+
+
+
+
+## Live Demo
+You can check out the live version of MovieMania: (https://) 
+
+
+## Features
+- Browse Popular, Top-Rated, and Upcoming Movies: Explore various categories of movies fetched from the TMDb API.
+- **Movie Details**: View details of each movie, including title, release date, synopsis, and ratings.
+- **Responsive Design**: Enjoy a seamless experience on different screen sizes.
+- **Easy Navigation**: Navigate between different sections smoothly using React Router.
+
+
+## Installation
+  To get a local copy up and running, follow these steps:
+
+  ### Prerequisites
+     Ensure you have Node.js and npm installed on your machine.
+
+  ### Steps
+     1. Clone the repository:
+        git clone https://github.com/ShyamSingh841442/Movie-Website.git
+
+     2. Navigate into the project directory:
+        cd Movie-Website
+
+     3. Install dependencies:
+        npm install
+
+     4. Start the development server:
+        npm start
+        The app will be available at http://localhost:3000 in your browser.
+
+## Technologies Used
+- React.js: For building the user interface.
+- React Router: For client-side routing.
+- CSS: For styling components.
+- TMDb API: For fetching movie data.
+
+## Project Structure
+   The project’s file structure is organized as follows:
+
+        movie_mania/
+        ├── public/
+        │   └── index.html
+        ├── src/
+        │   ├── assets/
+        │   │   └── MovieMania.png
+        │   ├── components/
+        │   │   ├── card/
+        │   │   │   ├── Card.js
+        │   │   │   └── Card.css
+        │   │   ├── header/
+        │   │   │   ├── Header.js
+        │   │   │   └── Header.css
+        │   │   ├── movieList/
+        │   │   │   ├── MovieList.js
+        │   │   │   └── MovieList.css
+        │   ├── pages/
+        │   │   ├── home/
+        │   │   │   ├── Home.js
+        │   │   │   └── Home.css
+        │   │   └── movieDetail/
+        │   │       ├── Movie.js
+        │   │       └── Movie.css
+        │   ├── App.js
+        │   ├── App.css
+        │   └── index.js
+        ├── .gitignore
+        ├── package.json
+        └── README.md
+
+
+## Usage
+  Homepage: Displays a list of popular movies.
+  
+  Categories: Explore different movie categories (Popular, Top Rated, Upcoming).
+  
+  Movie Details: Click on any movie to view detailed information about it.
+
+
+## Project Repository
+   The source code for MovieMania is available on GitHub: https://github.com/ShyamSingh841442/Movie-Website.git
+
+## Credits
+   The Movie Database (TMDb): Movie data API used to fetch movie details.
+   
+   React Icons: Used for adding icons across the UI.
+   
+   Create React App: For bootstrapping the project.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
+
+
+## Contact Information
+For any questions or feedback, feel free to reach out:
+
+- Name: Shyam Kumar (BTech Student, CSE)
+- Email: [shyamsingh841442@gmail.com](mailto:shyamsingh841442@gmail.com)  
+- LinkedIn: [Shyam Kumar LinkedIn](https://www.linkedin.com/in/shyamsingh841442)
+
+
+Thank you for checking out MovieMania! Enjoy exploring the world of movies.
---
+++ src/pages/movieDetail/Movie.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from "react";
+import "./Movie.css";
+import { useParams } from "react-router-dom";
+
+const Movie = () => {
+  const [currentMovieDetail, setMovie] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getData = () => {
+      fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=c8d73be48babf0abccb382ece420ae57&language=en-US`
+      )
+        .then((res) => res.json())
+        .then((data) => setMovie(data));
+    };
+
+    getData();
+    window.scrollTo(0, 0);
+  }, [id]); 
+  return (
+    <div className="movie">
+      <div className="movie__intro">
+        <img
+          className="movie__backdrop"
+          src={`https://image.tmdb.org/t/p/original${
+            currentMovieDetail ? currentMovieDetail.backdrop_path : ""
+          }`}
+          alt="Movie poster"
+        />
+      </div>
+      <div className="movie__detail">
+        <div className="movie__detailLeft">
+          <div className="movie__posterBox">
+            <img
+              className="movie__poster"
+              src={`https://image.tmdb.org/t/p/original${
+                currentMovieDetail ? currentMovieDetail.poster_path : ""
+              }`}
+              alt="Movie poster"
+            />
+          </div>
+        </div>
+        <div className="movie__detailRight">
+          <div className="movie__detailRightTop">
+            <div className="movie__name">
+              {currentMovieDetail ? currentMovieDetail.original_title : ""}
+            </div>
+            <div className="movie__tagline">
+              {currentMovieDetail ? currentMovieDetail.tagline : ""}
+            </div>
+            <div className="movie__rating">
+              {currentMovieDetail ? currentMovieDetail.vote_average : ""}{" "}
+              <i className="fas fa-star" />
+              <span className="movie__voteCount">
+                {currentMovieDetail
+                  ? "(" + currentMovieDetail.vote_count + ") votes"
+                  : ""}
+              </span>
+            </div>
+            <div className="movie__runtime">
+              {currentMovieDetail
+                ? currentMovieDetail.runtime + " mins"
+                : ""}
+            </div>
+            <div className="movie__releaseDate">
+              {currentMovieDetail
+                ? "Release date: " + currentMovieDetail.release_date
+                : ""}
+            </div>
+            <div className="movie__genres">
+              {currentMovieDetail &&
+                currentMovieDetail.genres &&
+                currentMovieDetail.genres.map((genre) => (
+                  <span className="movie__genre" key={genre.id}>
+                    {genre.name}
+                  </span>
+                ))}
+            </div>
+          </div>
+          <div className="movie__detailRightBottom">
+            <div className="synopsisText">Synopsis</div>
+            <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
+          </div>
+        </div>
+      </div>
+      <div className="movie__links">
+        <div className="movie__heading">Useful Links</div>
+        {currentMovieDetail && currentMovieDetail.homepage && (
+          <a
+            href={currentMovieDetail.homepage}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            <p>
+              <span className="movie__homeButton movie__Button">
+                Homepage <i className="newTab fas fa-external-link-alt"></i>
+              </span>
+            </p>
+          </a>
+        )}
+        {currentMovieDetail && currentMovieDetail.imdb_id && (
+          <a
+            href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            <p>
+              <span className="movie__imdbButton movie__Button">
+                IMDb <i className="newTab fas fa-external-link-alt"></i>
+              </span>
+            </p>
+          </a>
+        )}
+      </div>
+      <div className="movie__heading">Production companies</div>
+      <div className="movie__production">
+        {currentMovieDetail &&
+          currentMovieDetail.production_companies &&
+          currentMovieDetail.production_companies.map((company) => (
+            company.logo_path && (
+              <span className="productionCompanyImage" key={company.id}>
+                <img
+                  className="movie__productionCompany"
+                  src={
+                    "https://image.tmdb.org/t/p/original" + company.logo_path
+                  }
+                  alt={company.name}
+                />
+                <span>{company.name}</span>
+              </span>
+            )
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
